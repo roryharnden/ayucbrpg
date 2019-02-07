6ecdff5ae01acfc498cae01e7286112d6f06a466
roryharnden <roryharnden@users.noreply.github.com>
2019-02-07 21:57:07 +1300

    card update + random tweak

--- cards/random.js
+++ cards/random.js
@@ -5,19 +5,27 @@ link.addEventListener("click", shuffle);
 function shuffle(){
 
 
-  var imgCount = 54;
+  var imgCount = 48;
   var dir = "images/";
   var count = 16;
   var skewAmount = 5;
-  var headCount = Math.round(Math.random() * (imgCount - 1)) + 1;
+  var headCount = Math.round(Math.random() * (count -1 )) + 1;
   document.getElementById("head").style.backgroundImage = "url(" + dir + headCount + ".jpg)";
   document.getElementById("head").style.transform = "rotate(" + (Math.round(Math.random() * (+ skewAmount - 1)) - (0.5 * skewAmount)) + "deg)";
-  var midCount = Math.round(Math.random() * (imgCount - 1)) + 1 + count;
+
+  console.log("headCount = " + headCount);
+
+  var midCount = Math.round(Math.random() * (count -1)) + count +1;
   document.getElementById("mid").style.backgroundImage = "url(" + dir + midCount + ".jpg)";
   document.getElementById("mid").style.transform = "rotate(" + (Math.round(Math.random() * (+ skewAmount - 1)) - (0.5 * skewAmount)) + "deg)";
-  var footCount = Math.round(Math.random() * (imgCount - 1)) + 1 + count + count;
+
+  console.log("midCount = " + midCount);
+
+  var footCount = Math.round(Math.random() * (count -1)) + count + count +1;
   document.getElementById("foot").style.backgroundImage = "url(" + dir + footCount + ".jpg)";
   document.getElementById("foot").style.transform = "rotate(" + (Math.round(Math.random() * (+ skewAmount - 1)) - (0.5 * skewAmount)) + "deg)";
+
+  console.log("footCount = " + footCount);
 
 };
 shuffle();
