ebfa70573e79bcba243a333c7852eab30e315d66
roryharnden <roryharnden@users.noreply.github.com>
2020-11-24 21:30:17 +1300

    map updates

--- map/2x2/bg.js
+++ map/2x2/bg.js
@@ -1,13 +1,15 @@
 function random_bg_color() {
 		var dataHsb = document.body.dataset.hsb.split(' ');
 
-		if (Math.random() < 0.2){
-	    hueColor = Math.floor(Math.random() * 10);
-		} else {
-			hueColor = 90 + Math.floor(Math.random() * 270);
-		}
-    var hueSat = +dataHsb[1] + Math.floor(Math.random() * 15);
-    var hueLight = +dataHsb[2] + Math.floor(Math.random() * 10);
+		// if (Math.random() < 0.2){
+	  //   hueColor = Math.floor(Math.random() * 10);
+		// } else {
+		// 	hueColor = 90 + Math.floor(Math.random() * 270);
+		// }
+		hueColor = 180 + Math.floor(Math.random() * 60);
+
+    var hueSat = +dataHsb[1] + Math.floor(Math.random() * 20);
+    var hueLight = +dataHsb[2] + Math.floor(Math.random() * 5);
     var bgColor = "hsl(" + hueColor + "," + hueSat + "% ," + hueLight + "%)";
 
     console.log(bgColor);
